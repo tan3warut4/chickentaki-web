49679af5c3e82645a3c3f3de8b2bdf63bc81e3c0
tan3warut4 <itanprscritical@gmail.com>
2025-06-01 15:32:16 +0700

    Order logic

--- components/Order.tsx
+++ components/Order.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from './ui/card'
 import { Button } from './ui/button'
@@ -15,46 +16,51 @@ const Order = () => {
         }
         return false
     }
+    const noOrder = <div className='flex justify-center mt-15'> you didn't have any order yet.</div>
+
     return (
-        <div>
-            <Card>
-                <CardHeader>
-                    <CardTitle>Korea chicken </CardTitle>
-                    {items.map((item) => {
-                        return (
-                            <CardDescription key={item.name} className='mt-4'>
-                                <div className='flex justify-between'>
-                                    <div><p>{item.name}
-                                    </p></div>
-                                    <div>
-                                        <Button variant="addOrder" size={"addOrder"} disabled={menuDisable(item.quantity)} onClick={() => dispatch(decreaseItem(item.name))}>-</Button>
-                                        <span className='pr-1'> {item.quantity}</span>
-                                        <Button variant="addOrder" size={"addOrder"} onClick={() => dispatch(increaseItem(item.name))}>+</Button>
-                                        <DropdownMenu>
-                                            <DropdownMenuTrigger> <Button variant="outline" size="option">:</Button></DropdownMenuTrigger>
-                                            <DropdownMenuContent>
-                                                <DropdownMenuItem variant='delete'>Delete</DropdownMenuItem>
-                                            </DropdownMenuContent>
-                                        </DropdownMenu>
+        items.length != 0 ?
+            <div>
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Your order </CardTitle>
+                        {items.map((item) => {
+                            return (
+                                <CardDescription key={item.name} className='mt-4'>
+                                    <div className='flex justify-between'>
+                                        <div><p>{item.name}
+                                        </p></div>
+                                        <div>
+                                            <Button variant="addOrder" size={"addOrder"} disabled={menuDisable(item.quantity)} onClick={() => dispatch(decreaseItem(item.name))}>-</Button>
+                                            <span className='pr-1'> {item.quantity}</span>
+                                            <Button variant="addOrder" size={"addOrder"} onClick={() => dispatch(increaseItem(item.name))}>+</Button>
+                                            <DropdownMenu>
+                                                <DropdownMenuTrigger>
+                                                    <span className="cursor-pointer px-2 py-1 bg-gray-200 rounded-full hover:bg-gray-300">⋮</span>
+                                                </DropdownMenuTrigger>
+                                                <DropdownMenuContent>
+                                                    <DropdownMenuItem variant='delete'>Delete</DropdownMenuItem>
+                                                </DropdownMenuContent>
+                                            </DropdownMenu>
+                                        </div>
                                     </div>
-                                </div>
-                            </CardDescription>
-                        )
-                    })}
+                                </CardDescription>
+                            )
+                        })}
 
-                </CardHeader>
-                <CardContent className='my-4'>
-                    <div className='flex justify-between'>
-                        <div><p>Subtotal</p></div>
-                        <div>{total}</div>
-                    </div>
+                    </CardHeader>
+                    <CardContent className='my-4'>
+                        <div className='flex justify-between'>
+                            <div><p>Subtotal</p></div>
+                            <div>{total}</div>
+                        </div>
 
-                </CardContent>
-                <CardFooter className='mt-2'>
-                    <Button className='w-full' variant={"default"} >Generate a payment</Button>
-                </CardFooter>
-            </Card>
-        </div>
+                    </CardContent>
+                    <CardFooter className='mt-2'>
+                        <Button className='w-full' variant={"default"} >Generate a payment</Button>
+                    </CardFooter>
+                </Card>
+            </div> : noOrder
     )
 }
 
